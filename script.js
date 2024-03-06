b79177c33abc785ac91906dd9bd3cba54cf69b96
ferndz <nafernandez@mi.unc.edu.ar>
2024-03-06 20:38:30 -0300

    created Game Object

---
+++ script.js
@@ -0,0 +1,43 @@
+function createGame(){
+    let turn = 0;
+    let actualPlayer = 'Player 1';
+    let board = ['-','-','-',
+                 '-','-','-',
+                '-','-','-'];
+    const getTurn = () => turn;
+    const addTurn = () => {
+        turn++;
+        if(actualPlayer = 'Player 1'){
+            actualPlayer = 'Player 2';
+        }
+        else{
+            actualPlayer = 'Player 1';
+        }
+    }
+    const getActualPlayer = () => actualPlayer;
+    const playX = (position) => {
+        console.assert(board[position] === '-'); 
+        board[position] = 'X';
+    }
+    const playO = (position) => {
+        console.assert(board[position] === '-');
+        board[position] = 'O';
+        
+    }
+    return {board, turn, actualPlayer, getTurn, addTurn, getActualPlayer,
+            playX, playO}
+}
+
+const gameboard = createGame();
+
+function checkDiagonals(gameboard){
+
+}
+
+function checkRows(gameboard){
+
+}
+
+function checkColumns(gameboard){
+
+}
